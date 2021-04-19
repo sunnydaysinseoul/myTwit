12d3fe22aa8e42d7bee266d5ccffce1d92f3a487
thesunny-dev <59652037+thesunny-dev@users.noreply.github.com>
2021-04-19 13:39:18 +0900

    Update settings.py
    line23, hid SECRET_KEY

--- myTwit/settings.py
+++ myTwit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1igje=%mp@k=y=m(nq50$+qy35_brh!@#ex&nq78tc2sm_tiay'
+SECRET_KEY = 'hidden'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
